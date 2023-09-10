ce3a5aa868b238edcbccc94a3f1d5f59d1d8299f
leesochay <sochaylee@gmail.com>
2023-09-10 00:47:26 -0400

    updating config and package json

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,9 @@
-const mongoose = require('mongoose');
+// connect to the MongoDB database
+const { connect, connection } = require('mongoose');
 
-// Wrap Mongoose around local connection to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/mygroceryDB');
+const connectionString = 
+    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialMediaDB';
 
-// Export connection
-module.exports = mongoose.connection;
+connect(connectionString);
+
+module.exports = connection;
