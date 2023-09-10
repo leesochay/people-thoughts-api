a0642ee09a48ba1fd1231fb2b8aeca66ad3f259c
leesochay <sochaylee@gmail.com>
2023-09-09 20:18:40 -0400

    update model js files

--- models/Thought.js
+++ models/Thought.js
@@ -3,25 +3,33 @@ const reactionSchema = require('./Reaction');
 
 const thoughtSchema = new Schema(
   {
-    thoughtText: {type: String, required:true, maxlength:280,
+    thoughtText: {
+		type: String,
+		required: true,
+		maxlength: 280,
     },
-    createdAt: {type: Date, default: Date.now,
+		createdAt: {
+			type: Date,
+			default: Date.now,
     },
-    username: {type: String, required: true,
+		username: {
+			type: String,
+			required: true,
     },
-    reactions: [reactionSchema],
-  },
-  {
+		reactions: [reactionSchema],
+	},
+	{
     toJSON: {
       virtuals: true,
+	  getters: true,
     },
     id: false,
-  }
+	}
 );
 
 thoughtSchema.virtual('reactionCount').get(function () {
     return this.reactions.length;
-  });
+});
 
 const Thought = model('thought', thoughtSchema);
 
