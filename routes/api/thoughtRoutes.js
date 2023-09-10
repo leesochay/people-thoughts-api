a1f93de42c2e9fad2442fbdeb638697cfebcc2b6
leesochay <sochaylee@gmail.com>
2023-09-10 00:04:16 -0400

    updating api routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -5,12 +5,20 @@ const {
     createThought,
     deleteThought,
     updateThought,
+	createReaction,
+	removeReaction,
   } = require('../../controllers/thoughtController');
 
-  // /api/thoughts
+// thoughts
 router.route('/').get(getAllThoughts).post(createThought);
 
-// /api/thoughts/:thoughtId
+// thoughts/:thoughtId
 router.route('/:thoughtId').get(getSingleThought).delete(deleteThought).put(updateThought);
+
+// thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(createReaction);
+
+// thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction)
 
 module.exports = router;
